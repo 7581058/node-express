7bb4bacfa7832385b40b84aab232299c089b0a7e
daseul <7581058@gmail.com>
2024-04-08 01:33:24 +0900

    👍 Study: express, nodemon

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express")
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send("hello node")
+})
+app.get('/contacts', (req, res) => {
+  res.send("Contacts Page")
+})
+
+app.listen(3000, () => {
+  console.log("서버 실행 중")
+})
